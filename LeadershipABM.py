ee456fc100d5ef55de5c7cb18e822fe02101c01d
smsri <smsridharan@ucdavis.edu>
2019-04-17 19:58:33 -0700

    04/17/2019 - getElongation() and related functions

--- LeadershipABM.py
+++ LeadershipABM.py
@@ -47,6 +47,7 @@ def main(N,I):
     # steps = input("Please enter the number of steps for which the simulation should run: ")
     # steps = int(steps)
     steps = 100
+
     all_positions = dict()
 
     targetDestination_list = []
@@ -66,13 +67,13 @@ def main(N,I):
         animal_list.append(Animal('Uninformed' + str(num), np.array([uniform(-5, 5), uniform(-5, 5)]), randomDirection(), rand_speed, ss.vonmises.rvs(10000, size = 1)))
 
 
+    all_directions = {i: list() for i in range(0, steps)}
 
     agentNames = [o.name for o in animal_list]
     all_agents = {i: list() for i in agentNames}
-
-
     for i in range(steps):
         all_positions[i] = [o.position for o in animal_list]
+        all_directions[i] = np.sum([o.direction for o in animal_list], axis=0)
         for o in animal_list:
             all_agents[o.name].append(o.position)
         for animal in animal_list:
@@ -81,8 +82,7 @@ def main(N,I):
             animal.position = animal.new_position
             animal.direction = animal.new_direction
 
-    #return(all_positions)
-    return (all_positions, all_agents)
+    return (all_positions, all_agents, all_directions)
 
 
 
@@ -160,6 +160,30 @@ def randomDirection():
 def getPositions(animal_list):
     return [o.position for o in animal_list]
 
+def getElongation(positions, direction):
+    x_coords = [p[0] for p in positions]
+    y_coords = [p[1] for p in positions]
+    _len = len(positions)
+    centroid_x = sum(x_coords) / _len
+    centroid_y = sum(y_coords) / _len
+    centroid = [centroid_x, centroid_y]
+
+    direction_point = np.sum([direction, centroid], axis=0)
+    wdistances = [getDistFromLine(centroid, direction_point, position) for position in positions]
+    perpendicular_point = [centroid[0]-direction_axis[1], direction_axis[0]+centroid[1]]
+    ldistances = [getDistFromLine(centroid, perpendicular_point, position) for position in positions]
+
+
+def getDistFromLine(p1, p2, p3):   ##p1 and p2 are points on the line
+    dist = ((p2[0]-p1[0])*(p1[1]-p3[1]) - (p1[0]-p3[0])*(p2[1]-p1[1]))/np.sqrt(np.square(p2[0]-p1[0])+np.square(p2[1]-p1[1]))
+    #if (p3[1]<p1[1]+(p1[0]-p3[0])*((p2[1]-p1[1])/(p2[0]-p1[0]))):
+        #return dist * -1
+    return dist
+
+
+
+
+
 # this small function helps us determine the inherent direction of each animal in animalList
 def getDirections(animal_list):
     return [o.direction for o in animal_list]
@@ -230,13 +254,13 @@ def allGraphs(all_positions, steps):
 #         numInformed = np.int_(numInformed)
 #         return(numInformed)
 
-all_data = {10:{},30:{},50:{},100:{},200:{}} #initializing a list with size 5 because N = 10,30,50,100, 200 in Couzin paper
+##all_data = {10:{},30:{},50:{},100:{},200:{}} #initializing a list with size 5 because N = 10,30,50,100, 200 in Couzin paper
 
 
-for N in [10,30,50,100,200]:
-    print("Entering: N = ", N)
-    for I in range(1,N+1):
-        all_data[N][I] = main(N, I)
+##for N in [10,30,50,100,200]:
+##    print("Entering: N = ", N)
+##    for I in range(1,N+1):
+##        all_data[N][I] = main(N, I)
 
 
 
